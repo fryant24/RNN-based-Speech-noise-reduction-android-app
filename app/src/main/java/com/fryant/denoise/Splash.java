ffeb113f6e0fea7f9b313cdbf3ee8a80b54bacc1
Kobe Fryant <thufangli@163.com>
2019-02-25 21:35:49 +0800

    first commit

---
+++ app/src/main/java/com/fryant/denoise/Splash.java
@@ -0,0 +1,35 @@
+package com.fryant.denoise;
+
+import android.content.Intent;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.view.WindowManager;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);//隐藏状态栏
+        getSupportActionBar().hide();//隐藏标题栏
+        setContentView(R.layout.activity_splash);
+        getWindow().setBackgroundDrawableResource(R.drawable.splashgo);
+        Thread myThread = new Thread() {//创建子线程
+            @Override
+            public void run() {
+                try {
+                    sleep(4000);//使程序休眠四秒
+                    Intent it = new Intent(getApplicationContext(), MainActivity.class);//启动MainActivity
+                    startActivity(it);
+                    finish();//关闭当前活动
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();//启动线程
+    }
+}
